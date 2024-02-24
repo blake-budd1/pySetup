69be87a64a5dd670e5b1f24f06e627f0ea1c6e7b
alexacole <ajc19161@gmail.com>
2024-02-24 08:43:47 -0500

    Code polling website

---
+++ poll_website.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+website = "https://the-modern-jukebox-react-app.vercel.app/api/controls"
+
+previous = "null"
+
+while(1):
+    r = requests.get(website)
+    if(previous != r.text and r.text != "null"):
+        previous = r.text
+        controls = json.loads(r.text)
+        if(controls["play"]):
+            f = open("remote.txt", "a")
+            f.write("play\n")
+            f.close()
+        elif(controls["pause"]):
+            f = open("remote.txt", "a")
+            f.write("pause\n")
+            f.close()
+        elif(controls["next"]):
+            f = open("remote.txt", "a")
+            f.write("next\n")
+            f.close()
+        elif(controls["previous"]):
+            f = open("remote.txt", "a")
+            f.write("previous\n")
+            f.close()
+        
