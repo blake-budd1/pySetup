047613ac4963a3f2863d2606242ec68db1a0b37d
Blake Budd <70623089+blake-budd1@users.noreply.github.com>
2024-02-25 01:11:02 -0500

    Update queue_handling_v3.py

--- testing/python_scripts/queue_handling_v3.py
+++ testing/python_scripts/queue_handling_v3.py
@@ -17,25 +17,67 @@ SYSTEM_QUEUE_CONTROLLER = "../system_queue_controller.txt"
 SYSTEM_SFML_CONTROLLER = "../system_sfml_controller.txt"
 
 def readController():
+    sleep(5)
     command = ""
     with open(SYSTEM_QUEUE_CONTROLLER, "r") as controller_file:
             controller = controller_file.readlines()
-            if(controller):
-                command = controller[0].strip()
-                with open(SYSTEM_QUEUE_CONTROLLER, 'w') as controller_file:
-                    controller_file.writelines((controller[1:]))
-                print("controller = ", command)
+            controller_file.close()
+    if(controller):
+        command = controller[0].strip()
+        with open(SYSTEM_QUEUE_CONTROLLER, 'w') as controller_file:
+            controller_file.writelines((controller[1:]))
+            controller_file.close()
+        print("controller in queue handling = ", command)
     return command
 
 def writeToSystemController(command, file_path):
     # Clear the system controller
     with open(file_path,"w") as file:
         file.truncate(0)
+        file.close()
     # Write the new command to the system controller
     with open(file_path, "w") as file:
         file.write(command)
+        file.close()
     print("wrote: ", command, "to system controller")
 
+# updated on 2/24 to post new song to front end when something other than time ran out
+def updateCurrentFrontendSong():
+    sleep(15)
+    # Need to go through the current_song file, (this is called after that all is handled properly)
+    # curr_play_ur = {"message": data[0]}
+    #         post_response = requests.post(CURR_URL, json=curr_play_ur, headers={'Content-Type': 'application/json'})
+    #         post_response.raise_for_status()
+    with open(CURRENT_PATH, "r") as file:
+        lines = [line.strip() for line in file]
+    file.close()
+    # if(lines):
+    #     lines_to_post = lines[:-1] # remove the last line   
+    
+    data = {
+        "uri":lines[1],
+        "userAccessToken":lines[2],
+        "duration":lines[6],
+        "trackName":lines[3],
+        "trackArtist":lines[4],
+        "trackCover":lines[5]
+    }
+    
+    json_data =json.dumps({"message":data})
+    headers = {'Content-type':'application/json'}
+    response = requests.post(CURR_URL, json=json_data, headers=headers)
+    # response = requests.post(CURR_URL, json =data)
+    response.raise_for_status()
+
+def checkEmpty(file):
+    with open(file, 'r') as file_obj:
+        first_char = file_obj.read(1)
+        file_obj.close()
+        if not first_char:
+            return True # True means file is empty
+        else:
+            return False
+        
 
 def get_current_song():
     try:
@@ -44,8 +86,8 @@ def get_current_song():
         get_list.raise_for_status()
 
         data = get_list.json()
-        for item in data:
-            print(item)
+        # for item in data:
+        #     print(item)
         if data:
             data_size  = len(data)
             
@@ -79,7 +121,7 @@ def get_current_song():
                 file.write(current_item["trackCover"] + "\n")
                 file.write(str(current_item["duration"]) + "\n")
                 file.write(str(duration_ms) + "\n")
-
+                file.close()
             # Write information to the file holding the data for the next song to be played
             if data_size > 1:
                 print("writing next song data with 2\n")
@@ -92,6 +134,7 @@ def get_current_song():
                     file.write(next_item["trackCover"] + "\n")
                     file.write(str(next_item["duration"]) + "\n")
                     file.write(str(duration_ms) + "\n")
+                    file.close()
             else:
                 open(NEXT_PATH, 'w').close()                   
             # Write the duration_sec to the file
@@ -168,6 +211,7 @@ def update_album_cover(data):
     # Initial data when system is powered on
 data = get_current_song() # Handles reading and writing the current_song and the next_song (if available)
 update_album_cover(data)
+writeToSystemController("update", SYSTEM_SFML_CONTROLLER)
     # sleep_duration = float(open(CURRENT_SLEEP_PATH, "r").read().strip())
     # print("sleeping for : " + str(sleep_duration - 5)+ "\n")
     # sleep(sleep_duration - 5)
@@ -189,14 +233,15 @@ while True:
         # Set the current song to the next song
         with open(NEXT_PATH, "r") as next_file:
             next_song_local = next_file.readlines()
-
+            next_file.close()
         # Set the current song to be the song in next_song (old current song)
         with open(CURRENT_PATH, "w") as curr_file:
             curr_file.writelines(next_song_local)
-
+            curr_file.close()
         # Set the next song to be the next song before the previous
         with open(NEXT_PATH, "w") as next_file:
             next_file.writelines(next_song)
+            next_file.close()
         prev_count -= 1
         # Note that we are done handling previous
         if prev_count == 0:
@@ -204,9 +249,13 @@ while True:
         # Need to write the new current sleep time
         with open(CURRENT_SLEEP_PATH, "w") as sleep_file:
             sleep_file.write(next_song_local[7])
-
+            sleep_file.close()
+        
         # Tell SFML to update
         writeToSystemController("update", SYSTEM_SFML_CONTROLLER)
+
+        # Update front end song
+        # updateCurrentFrontendSong()
 
         # clear next song so the next previous can use is
         next_song = ""
@@ -214,8 +263,11 @@ while True:
     elif queue_command == "previous":
         # need to replace the current song with the previous song
         # Read in the previous song file (the most recent one) and remove it from the file
+       
+            
         with open(PREVIOUS_PATH, "r") as prev_file:
             prev_content = prev_file.readlines()
+            prev_file.close()
         # Store the previous song
         print("previous songs: " , prev_content)
         prev_song = prev_content[:8]
@@ -224,37 +276,42 @@ while True:
         # Rewrite previous song data without the new current song
         with open(PREVIOUS_PATH, "w") as prev_file:
             prev_file.writelines(prev_content[8:])
+            prev_file.close()
 
         # Read in the current song and update it to be next song
         with open(CURRENT_PATH, "r") as curr_file:
             curr_song = curr_file.readlines()
+            curr_file.close()
 
         # Clear the current song file
         with open(CURRENT_PATH, "w") as curr_file:
             curr_file.truncate(0)
-        
+            curr_file.close()
         # write the new current song to its file
         with open(CURRENT_PATH, "w") as curr_file:
             curr_file.writelines(prev_song)
-        
+            curr_file.close()
         with open(CURRENT_SLEEP_PATH, "w") as sleep_file:
             sleep_file.write(prev_song[7])
-
+            sleep_file.close()
         # Store the next song to use the next loop
         with open(NEXT_PATH, "r") as next_file:
             next_song = next_file.readlines()
             print("next_song data: ", next_song)
-
+            next_file.close()
         # Write the old current song as the next song:
         with open(NEXT_PATH, "w") as next_file:
             next_file.writelines(curr_song)
-        
+            next_file.close()
         # Set prev_handled = False (we will have to skip getting the queue one more time because we already have the next song)
         prev_handled = True
         prev_count += 1
 
+        
         # Tell sfml to update
         writeToSystemController("update", SYSTEM_SFML_CONTROLLER)
+        # Update the song on front end
+        # updateCurrentFrontendSong()
 
         
         
