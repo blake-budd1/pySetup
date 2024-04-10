92b5163bf387342d091ee691228f2d1af97fa429
alexacole <62025175+alexacole@users.noreply.github.com>
2024-04-10 16:47:08 -0400

    Update install.py

--- install.py
+++ install.py
@@ -2,26 +2,18 @@
 sudo apt-get -y install curl && curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
 
 # Spotipy installation
-pip install spotipy
-pip install spotipy --upgrade
+pip install spotipy --upgrade --break-system-packages
 
 # OpenCV installation
-pip install opencv-python
-pip install opencv-contrib-python
+pip install opencv-python --break-system-packages
+pip install opencv-contrib-python --break-system-packages
 
 # SFML Installation
-sudo apt-get install libsfml-dev
+sudo apt-get install libsfml-dev 
 
 # Remote Packages install
 sudo apt-get install cec-utils
 sudo apt-get install xdotool
 
-# Creating the directory structure for the project:
-mkdir -p /home/pi/modern_jukebox
-mkdir -p /home/pi/modern_jukebox/temp_files
-mkdir -p /home/pi/modern_jukebox/python_files
-mkdir -p /home/pi/modern_jukebox/c_files
-
 # Create the necessary files for file storage on the device
-cd /home/pi/modern_jukebox/temp_files
-touch current_song.txt next_song.txt next_songs.txt previous_songs.txt sfml_controller.txt
+# Add this back eventually if we need it
